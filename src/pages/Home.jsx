e93e702a19326be69ba9751a7022e6cea725a8c3
PearceKof <benjaminlaurentpro00@gmail.com>
2024-11-26 15:12:41 +0100

    fix ortho

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,15 @@
-
-
 function Home () {
     return (
         <div>
 
             <div className="w-96">
             <h1 className="text-4xl text-white mb-6">Home</h1>
-                <p>Bienvenue Laurent De Plaen dans cette petite démo. Mettez vous à l'aise.</p>
-                <p>Je l'ai réalisée avec Vite + react et j'ai utilisé tailwind CSS pour le style. J'ai aussi expérimenté quelques outils de Material UI que je n'avais jamais utilisé avant</p>
+                <p>Bienvenue Laurent De Plaen dans cette petite démo. Mettez-vous à l'aise.</p>
+                <p>Je l'ai réalisée avec Vite + React et j'ai utilisé Tailwind CSS pour le style. J'ai aussi expérimenté quelques outils de Material UI que je n'avais jamais utilisés avant.</p>
                 <h1 className="text-2xl text-white mt-16 mb-6">À propos de moi</h1>
-                <p>Après avoir obtenu mon cess via le jury centrale. J'ai commencé des études d'infographie à La Haute École Albert Jackard. Après une année qui ne m'a pas convaincue, étant donné que les cours qui m'intéressaient le plus étaient la programmation, j'ai décidé de me lancer dans le cursus de 19. Une insitution qui propose une formation atypique basé sur des projets et une méthode d'aprentissage peer to peer.</p>
-                <p>Le dernier projet que j'ai rendu à 19 est un site web en react dans lequel il et possible de jouer à pong. C'est un travail de groupe. On s'est donc réparti les tâches. J'ai travaillé spécifiquement sur toute la partie de gestion d'utilisateur, de connexion via le site lui même ou via la plateforme de 19.</p>
-                <p>Après 19, j'ai réalisé un stage de 4 mois dans l'entreprise Clinigen à Louvain-la-neuve où j'ai supporté l'équipe sur un projet en react.</p>
+                <p>Après avoir obtenu mon CESS via le jury central, j'ai commencé des études d'infographie à la Haute École Albert Jacquard. Après une année qui ne m'a pas convaincu, étant donné que les cours qui m'intéressaient le plus étaient la programmation, j'ai décidé de me lancer dans le cursus de 19. Une institution qui propose une formation atypique basée sur des projets et une méthode d'apprentissage peer-to-peer.</p>
+                <p>Le dernier projet que j'ai rendu à 19 est un site web en React dans lequel il est possible de jouer à Pong. C'est un travail de groupe. On s'est donc réparti les tâches. J'ai travaillé spécifiquement sur toute la partie gestion d'utilisateur, de connexion via le site lui-même ou via la plateforme de 19.</p>
+                <p>Après 19, j'ai réalisé un stage de 4 mois dans l'entreprise Clinigen à Louvain-la-Neuve où j'ai soutenu l'équipe sur un projet en React.</p>
                 <p></p>
             </div>
         </div>
