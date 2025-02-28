c83d275f32db92fceb7bedc8677828e6f06b5ed1
PearceKof <benjaminlaurentpro00@gmail.com>
2025-02-28 15:44:33 +0100

    cleanup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,30 @@ function Home () {
 
             <div className="w-96">
             <h1 className="text-4xl text-white mb-6">Home</h1>
-                <p>Bienvenue Laurent De Plaen dans cette petite démo. Mettez-vous à l'aise.</p>
+                <p>Bienvenue dans cette petite démo. Mettez-vous à l'aise.</p>
                 <p>Je l'ai réalisée avec Vite + React et j'ai utilisé Tailwind CSS pour le style. J'ai aussi expérimenté quelques outils de Material UI que je n'avais jamais utilisés avant.</p>
-                <h1 className="text-2xl text-white mt-16 mb-6">À propos de moi</h1>
-                <p>Après avoir obtenu mon CESS via le jury central, j'ai commencé des études d'infographie à la Haute École Albert Jacquard. Après une année qui ne m'a pas convaincu, étant donné que les cours qui m'intéressaient le plus étaient la programmation, j'ai décidé de me lancer dans le cursus de 19. Une institution qui propose une formation atypique basée sur des projets et une méthode d'apprentissage peer-to-peer.</p>
-                <p>Le dernier projet que j'ai rendu à 19 est un site web en React dans lequel il est possible de jouer à Pong. C'est un travail de groupe. On s'est donc réparti les tâches. J'ai travaillé spécifiquement sur toute la partie gestion d'utilisateur, de connexion via le site lui-même ou via la plateforme de 19.</p>
-                <p>Après 19, j'ai réalisé un stage de 4 mois dans l'entreprise Clinigen à Louvain-la-Neuve où j'ai soutenu l'équipe sur un projet en React.</p>
-                <p></p>
+                <h1 className="text-2xl font-semibold mt-10">Titre 1</h1>
+                <p className="text-gray-400">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac metus placerat, 
+                    tincidunt arcu sed, scelerisque massa. Integer euismod sapien ut libero suscipit, 
+                    at facilisis quam fermentum.
+                </p>
+
+                <h2 className="text-2xl font-semibold mt-10">Titre 2</h2>
+                <p className="text-gray-400">
+                    Vestibulum tincidunt lacus a ex vestibulum, et elementum purus viverra. 
+                    Suspendisse nec felis vel turpis vestibulum gravida ut in eros.
+                </p>
+
+                <h2 className="text-2xl font-semibold mt-10">Titre 3</h2>
+                <p className="text-gray-400">
+                    Fusce sed ipsum eu diam euismod commodo. Curabitur euismod justo at metus volutpat, 
+                    vitae tincidunt elit cursus. Donec in purus sed velit dictum dictum at eu nisl.
+                </p>
             </div>
         </div>
     );
 }
 
 export default Home;
+
